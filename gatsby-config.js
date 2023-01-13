fbd036340d180d037899b192fd032cd67bf1dfbd
pieh <misiek.piechowiak@gmail.com>
2023-01-13 11:15:50 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `ignore-peer-deps`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
